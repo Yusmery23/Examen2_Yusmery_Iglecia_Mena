f5d748e9c146ca4560decb76930c425b358a76f7
Yusmery Iglecia <148628304+Yusmery23@users.noreply.github.com>
2023-12-12 23:57:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Taller_Mecanico_IPChile>(conexion =>
+{
+    conexion.UseSqlServer(builder.Configuration.GetConnectionString("Taller_Mecanico_IPChile")});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
